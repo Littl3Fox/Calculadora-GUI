b08b9fa8b52a84252a50aad6344bab51ac73b978
Littl3Fox <102700820+Littl3Fox@users.noreply.github.com>
2025-04-26 13:11:04 -0300

    Todos os butões funcionam e algumas verificações foram implementadas

--- main.py
+++ main.py
@@ -1,61 +1,111 @@
 from tkinter import *
 from tkinter import ttk
+import re
+
+#Cria a janela
+root = Tk()
+#Cria o frame que ira receber os widgets(Componentes gráficos)
+frm = ttk.Frame(root, padding=10)
+frm.grid()
+
+#Cria o título da janela
+root.title("Calculadora")
+
+#Defino a variável que irá Atualizar minha Entry
+
+operacao = StringVar(frm)
+
+#Defino um valor padrão
+operacao.set("0")
+
+#Aqui eu verifico se os operadores podem ser utilizados e colocados na Entry
+def verifica(operador):
+
+    atual = operacao.get()
+
+    if operador in "+-*/":
+        # Não permitir operadores no começo
+        if not atual:
+            return
+        # Se o último caractere for operador, substitui pelo novo operador
+        if atual[-1] in "+-*/":
+            operacao.set(atual[:-1] + operador)
+        else:
+            operacao.set(atual + operador)
+        
+        #Não permite mais de um operador na operação
+        if all(op not in atual for op in ["/", "*", "-", "+"]):
+            return
+
+    elif operador == ",":
+        #Caso seja o primeiro número e ele não tem vírgula ainda adiciona uma vírgula
+        if all(op not in atual for op in ["/", "*", "-", "+"]) and atual != "":
+            if operador not in atual:
+                operacao.set(atual + operador)
+        #Caso seja o segundo número e ele não tem vírgula ainda adiciona uma vírgula
+        else:
+            partes = re.split(r'(\+|\-|\*|/)', atual)
+            print(partes)
+            if operador not in partes[2] and partes[2] != "":
+                operacao.set(atual+operador)
+
+    #TODO(Fazer esse negócio funcionar. Tem que verificar se a operação está correta e tals)            
+    elif operador == "=":
+        print(eval(operacao.get()))
+    else:
+        # Adiciona o operador
+        operacao.set(atual + operador)
+
+
+
+
 
 def main():
 
-    #Cria a janela
-    root = Tk()
-    #Cria o frame que ira receber os widgets(Componentes gráficos)
-    frm = ttk.Frame(root, padding=10)
-    frm.grid()
-
-    #Cria o título da janela
-    root.title("Calculadora")
-
     #Cria o espaço onde vai aparecer a operação do usuário e o resultado
-    ttk.Label(frm, text="O número vai aqui").grid(row=0)
+
+    ttk.Entry(frm,justify="right", textvariable = operacao).grid(row=0)
 
     #Cria o botão de apagar o número digitado
-    ttk.Button(frm, text="<-",).grid(column=1, row=1)
+    ttk.Button(frm, text="<-",command= lambda: operacao.set(operacao.get()[:-1] )).grid(column=1, row=1)
 
     #Cria o botão de resetar a operação
-    ttk.Button(frm, text="CE",).grid(column=2, row=1)
+    ttk.Button(frm, text="CE",command= lambda: operacao.set("")).grid(column=2, row=1)
 
     #Cria o botão de limpar a tela
-    ttk.Button(frm, text="C",).grid(column=3, row=1)
+    ttk.Button(frm, text="C",command= lambda: operacao.set("")).grid(column=3, row=1)
 
     #Cria o botão da operação de divisão
-    ttk.Button(frm, text="/",).grid(column=4, row=1)
+    ttk.Button(frm, text="/",command=lambda: verifica("/")).grid(column=4, row=1)
 
     #Cria os botões dos números
     numero = 1
     for linha in range(2,5):         # 3 linhas
         for col in range(1,4):       # 3 colunas
-            ttk.Button(frm, text=str(numero)).grid(column=col, row=linha)
+            ttk.Button(frm, text=str(numero),command= lambda n = numero: operacao.set(operacao.get() + str(n))).grid(column=col, row=linha)
             numero += 1
         
     #Cria o botão do número 0
-    ttk.Button(frm,text="0").grid(column=2,row=5)
+    ttk.Button(frm,text="0", command= lambda: operacao.set(operacao.get() + str(0))).grid(column=2,row=5)
 
     #Cria o botão da operação multiplicação
-    ttk.Button(frm,text="*").grid(column=4,row=2)
+    ttk.Button(frm,text="*",command=lambda: verifica("*")).grid(column=4,row=2)
 
     #Cria o botão da operação subtração
-    ttk.Button(frm,text="-").grid(column=4,row=3)
+    ttk.Button(frm,text="-",command=lambda: verifica("-")).grid(column=4,row=3)
 
     #Cria o botão da operação soma
-    ttk.Button(frm,text="+").grid(column=4,row=4)
+    ttk.Button(frm,text="+",command=lambda: verifica("+")).grid(column=4,row=4)
 
     #Cria o botão da operação resultado
-    ttk.Button(frm,text="=").grid(column=4,row=5)
+    ttk.Button(frm,text="=",command=lambda: verifica("=")).grid(column=4,row=5)
 
     #Cria o botão da vírgula
-    ttk.Button(frm,text=",").grid(column=3,row=5)
+    ttk.Button(frm,text=",",command=lambda: verifica(",")).grid(column=3,row=5)
 
 
     root.mainloop()
     return
-
 
 
 
